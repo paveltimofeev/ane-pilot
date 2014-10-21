0f78b7c669f287a400b4a9c24926fec938eed6f0
Pavel.Timofeev <pavel.timofeev@software.dell.com>
2014-10-21 16:33:15 +0400

    added Jasmine

---
+++ app/Jasmine/spec/test.js
@@ -0,0 +1,31 @@
+
+describe('Availability of views', function(){
+
+	it('Mailboxes view should exists', function(){
+	
+		
+		expect(true).toBe(true);
+	});
+	
+	it('Not implemented views should return corresponding message', function(){
+	
+		message = 'Info - This page is not implemented yet';
+		expect(message).toMatch('This page is not implemented yet');
+	});
+	
+});
+
+describe('Main action', function(){
+
+	it('"REFRESH" should fill the grid', function(){
+		
+		var gridRowsCountR = 100;
+		expect(gridRowsCountR).toBeGreaterThan(0);
+	});
+	
+	it('"RESET DATABASE" should clear the grid', function(){
+	
+		gridRowsCount = 0;
+		expect(gridRowsCount).toBe(0);
+	});
+});
